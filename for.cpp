ebae812fa4e8793d521074ce8bc274f35e02ed50
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-28 12:42:39 +0530

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,20 @@
+//Maitreyee Gohad
+//24070123509
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        cout<<"SIT"<<endl;
+    }
+}
+
+/* OUTPUT:
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
