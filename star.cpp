61816c54b680816545b594ffdda4bfb48edfb3df
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-29 17:52:35 +0530

    Create star.cpp

---
+++ star.cpp
@@ -0,0 +1,29 @@
+//Maitreyee Gohad
+//24070123509
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter your choice: ";
+    cin>>n;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+/* OUTPUT:
+Enter your choice: 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
